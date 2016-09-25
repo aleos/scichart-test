e7c9768acd498b7285cdaf574c71ad075cf736b0
Alexander Ostrovsky <thealeos@gmail.com>
2016-09-25 17:17:05 +0300

    + SciChart licensing

--- SciChartTest/AppDelegate.swift
+++ SciChartTest/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SciChart
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let licencing =
+            "<LicenseContract>"
+                + "<Customer>thealeos@gmail.com</Customer>"
+                + "<OrderId>Trial</OrderId>"
+                + "<LicenseCount>1</LicenseCount>"
+                + "<IsTrialLicense>true</IsTrialLicense>"
+                + "<SupportExpires>10/25/2016 00:00:00</SupportExpires>"
+                + "<ProductCode>SC-IOS-2D-ENTERPRISE-SRC</ProductCode>"
+                + "<KeyCode>32f5c8e3ce4afc02ab4a2435a5d92716b138bda373b598a5c5c6d5250a2dce75ef6be1f060fe1b835c2b2e272b956f9987b8615dfc55aeef0545137073671fade6a6231d20d5fc38dacdbab5e9fba111af3c2038c8093508870343ae64244cbb31f8586b634d75d9f3a16a01ece3253795bf5f62128c738da585486880ef88fc5f27a0975adfd4de03913b7ee1da77574bedf4aab356ceb38f39b82ee1fadd8d328f20d5f6143d97</KeyCode>"
+                + "</LicenseContract>"
+        
+        SCIChartSurface.setLicenseContract(licencing)
+        
         return true
     }
 
