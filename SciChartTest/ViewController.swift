b82c140e9586ea9d0aff22772717fc18242eea24
Alexander Ostrovsky <thealeos@gmail.com>
2016-09-25 16:56:06 +0300

    + empty project

---
+++ SciChartTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SciChartTest
+//
+//  Created by Alexander Ostrovsky on 25.09.16.
+//  Copyright © 2016 Alexander Ostrovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
